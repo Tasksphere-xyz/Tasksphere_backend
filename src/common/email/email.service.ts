583514ed96b532795aa08abafae1f78f263f3329
iamkhalidll <youngatere04@mgmail.com>
2025-11-16 23:41:04 +0100

    mail service bug fix

--- src/common/email/email.service.ts
+++ src/common/email/email.service.ts
@@ -8,18 +8,16 @@ export class EmailService {
 
   constructor() {
     this.transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      host: process.env.EMAIL_HOST,
+      port: Number(process.env.EMAIL_PORT),
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
       },
-       tls: {
-        rejectUnauthorized: false,
-      },
-      connectionTimeout: 10000,
-
+      tls: {
+        rejectUnauthorized: false
+      }
     } as TransportOptions);
   }
   async sendEmail(to: string, subject: string, html: string) {
