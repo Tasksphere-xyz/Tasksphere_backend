34e073e664358b2e30ce10126ce3c909e5c9b4d1
iamkhalidll <youngatere04@mgmail.com>
2025-11-16 22:23:50 +0100

    BUG FIX

--- src/modules/task/dto/update-task-status.dto.ts
+++ src/modules/task/dto/update-task-status.dto.ts
@@ -1,5 +1,4 @@
 import { IsEnum } from 'class-validator';
-
 export class UpdateTaskStatusDto {
   @IsEnum(['pending', 'in-progress', 'completed'], {
     message: 'Status must be either pending, in-progress, or completed',
